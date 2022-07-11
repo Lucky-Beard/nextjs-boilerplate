de2f6b3a8008009ca80e3feec6772ddd008ac710
Morné <morne@luckybeard.com>
2022-07-11 14:53:43 +0200

    Updated component

--- pages/component-list/index.tsx
+++ pages/component-list/index.tsx
@@ -1,18 +1,26 @@
 /* A PAGE DEDICATED FOR LISTING ALL COMPONENTS TO AVOID REDUNDANT COMPONENTS */
 
 
-import type { NextPage } from 'next';
+import { NextPageWithLayout } from '../_app';
 import styles from './index.module.scss';
+import Layout from '../../layouts/default';
 
-const ComponentList: NextPage = () => {
-
+const ComponentList: NextPageWithLayout = () => {
     return (
         <div className={styles.container}>
             <main>
-                This is a cool page
+                This page holds a list of all the components created so that duplicates can be avoided
             </main>
         </div>
     );
 };
+
+ComponentList.getLayout = function getLayout(page) {
+    return (
+      <Layout>
+        {page}
+      </Layout>
+    );
+  };
 
 export default ComponentList;
