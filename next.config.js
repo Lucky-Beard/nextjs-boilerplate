31d7440e37d08da7dd650aab550933ef35306dc3
Chantelle <chantelle@luckybeard.com>
2024-02-09 09:36:04 +0200

    Add mixins

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
 
+const prependData = [
+  '@import "./assets/styles/variables.scss";',
+  '@import "./assets/styles/mixins.scss";'
+];
+
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'assets/styles')],
-    prependData: '@import "./assets/styles/variables.scss";'
+    prependData: prependData.join('')
   }
 };
 module.exports = nextConfig;
